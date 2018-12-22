991bdebda2e0b35905de7aeb647f017e13d76149
DreIntheBay <drew@lumoslabs.com>
2018-12-22 00:23:51 -0500

    ISS-Location upload

---
+++ ISS-Location/ISS-Location.py
@@ -0,0 +1,22 @@
+import urllib3 as urllib
+import json
+import csv
+
+shttp =urllib.PoolManager()
+req = http.request('GET',"http://api.open-notify.org/iss-now.json")
+
+obj = json.loads(req.data.decode('utf-8'))
+
+
+print (obj['timestamp'])
+print (obj['iss_position']['latitude'])
+print (obj['iss_position']['longitude'])
+
+
+newRow = [obj['timestamp'],obj['iss_position']['latitude'],obj['iss_position']['longitude']]
+print (newRow)
+
+with open('location-logger.csv', 'a') as csvfile:
+	writer = csv.writer(csvfile)
+	writer.writerow(newRow)
+
