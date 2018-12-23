56bee78e67dd788c4b41120c81ba8163c38545cd
Denny <dendenlehman@gmail.com>
2018-12-23 15:42:49 -0600

    Create ISS_mapper.py
    created mapper class

---
+++ ISS_mapper.py
@@ -0,0 +1,17 @@
+'''
+doc string
+This class will connect to and process the ISS data
+It will do analysis and map the coordinates of the ISS
+'''
+
+import numpy as np
+import pandas as pd
+
+class Mapper():
+	"""docstring for ClassName"""
+	def __init__(self, csv_file):
+		self.csv_file = csv_file
+		self.df = pd.read_csv(self.csv_file)
+		
+	def connect(self):
+		pass
